8ac759a2153321a7e76ae1b315894ae4a08fdcbb
yperez-dot <yperez@healthexps.com>
2026-04-16 19:30:16 -0400

    Update bob.js

--- routes/bob.js
+++ routes/bob.js
@@ -6,8 +6,7 @@ const path = require('path');
 const fs = require('fs');
 const { getPool } = require('../db/database');
 const { requireAuth } = require('./auth');
-const { normalizeAgentName } = require('../normalize');
-
+const { normalizeAgentName } = require('./normalize');
 const UPLOADS_DIR = path.join('/tmp', 'uploads');
 if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR, { recursive: true });
 
