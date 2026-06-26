8563f0c033ee89d172d4d1d15dd0146ccdd7a773
yperez-dot <yperez@healthexps.com>
2026-06-26 14:18:15 -0400

    Update edit-commission.js

--- routes/edit-commission.js
+++ routes/edit-commission.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../db');
+require('../db/database')
 
 /**
  * Manual Edit Commission Record with Audit Trail
